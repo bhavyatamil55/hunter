bff79472922072c8b91ac80d8e605ae22e476bf1
bhavyatamil55 <51980999+bhavyatamil55@users.noreply.github.com>
2019-07-27 14:25:27 +0530

    Create 27.py

---
+++ 27.py
@@ -0,0 +1,13 @@
+sp=input()
+bh=len(sp)
+
+def check_palindrome(sp,bh):
+    if sp==sp[-1:-n-1:-1]:
+        bh-=1
+        sp=sp[0:bh]
+        check_palindrome(sp,bh)
+    else:
+        print(sp)
+
+
+check_palindrome(sp,bh)  
