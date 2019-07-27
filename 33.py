36511a01e6326c624953eb03dbc3b3535de57eb1
bhavyatamil55 <51980999+bhavyatamil55@users.noreply.github.com>
2019-07-27 14:50:02 +0530

    Create 33.py

---
+++ 33.py
@@ -0,0 +1,21 @@
+sp=input()
+las=[0]
+if "ab" not in sp:
+	print("0")
+else:
+	for i in range(len(sp)):
+		cak=1
+		for j in range(i,len(sp)-1):
+			if sp[j]=="a" and sp[j+1]=="b":
+				cak=cak+1
+			elif sp[j]=="b" and sp[j+1]=="a":
+				cak=cak+1
+			else:
+				las.append(cak)
+				cak=1
+				break
+		if sp[i]=="a":
+			las.append(cak)
+		else:
+			las.append(c-1)
+	print(max(las))
